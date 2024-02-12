ee87592f236b0fbf023ec98f9a2e97370e77df64
HAMOUDA\Hamouda <mohamed.hamouda7897@gmail.com>
2024-02-12 22:51:18 +0200

    remove this line

--- lib/home/tabs/radio_tab.dart
+++ lib/home/tabs/radio_tab.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class RadioTab extends StatelessWidget {
-  static const String routeName = "RadioTab";
 
    const RadioTab({super.key});
 
